03a90cb81a1dc27a76282707f9f9bd4049856a4c
linhuaxin <linhuaxin@foxmail.com>
2019-02-10 20:35:30 +0800

    BST

--- src/com/linhuaxin/ds/bst/BST.java
+++ src/com/linhuaxin/ds/bst/BST.java
@@ -46,4 +46,66 @@ public class BST<E extends Comparable<E>> {
 
         return node;
     }
+
+    // 看二分搜索树中是否包含元素e
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    // 看以node为根的二分搜索树中是否包含元素e，递归算法
+    private boolean contains(Node node, E e) {
+        if (node == null) {
+            return false;
+        }
+
+        if (e.compareTo(node.e) == 0) {
+            return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else {
+            return contains(node.right, e);
+        }
+    }
+
+    // 二分搜索树的前序遍历
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();
+    }
+
+    // 生成以node为根节点，深度为depth的描述二叉树的字符串
+    private void generateBSTString(Node node, int depth, StringBuilder res) {
+        if (node == null) {
+            res.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+
+        res.append(generateDepthString(depth) + node.e + "\n");
+        generateBSTString(node.left, depth + 1, res);
+        generateBSTString(node.right, depth + 1, res);
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            res.append("--");
+        }
+        return res.toString();
+    }
 }
---
+++ test/com/linhuaxin/ds/bst/BSTTest.java
@@ -0,0 +1,17 @@
+package com.linhuaxin.ds.bst;
+
+public class BSTTest {
+
+    public static void main(String[] args) {
+
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for (int num : nums) {
+            bst.add(num);
+        }
+        bst.preOrder();
+        System.out.println();
+
+        System.out.println(bst);
+    }
+}
