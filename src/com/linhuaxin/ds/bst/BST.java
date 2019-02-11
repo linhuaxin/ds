0bf6a5d03a095852b720751c48643d2778cbbf9d
linhuaxin <linhuaxin@foxmail.com>
2019-02-11 21:07:43 +0800

    BST

--- src/com/linhuaxin/ds/bst/BST.java
+++ src/com/linhuaxin/ds/bst/BST.java
@@ -1,5 +1,9 @@
 package com.linhuaxin.ds.bst;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BST<E extends Comparable<E>> {
 
     private class Node {
@@ -81,6 +85,24 @@ public class BST<E extends Comparable<E>> {
         preOrder(node.right);
     }
 
+    // 二分搜索树的非递归前序遍历
+    public void preOrderNR() {
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+    }
+
     // 二分搜索树的中序遍历
     public void inOrder() {
         inOrder(root);
@@ -107,6 +129,94 @@ public class BST<E extends Comparable<E>> {
         postOrder(node.left);
         postOrder(node.right);
         System.out.println(node.e);
+    }
+
+    public void levelOrder() {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node cur = q.remove();
+            System.out.println(cur.e);
+
+            if (cur.left != null) {
+                q.add(cur.left);
+            }
+            if (cur.right != null) {
+                q.add(cur.right);
+            }
+        }
+    }
+
+    // 寻找二分搜索树的最小元素
+    public E minimum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+        return minimum(root).e;
+    }
+
+    // 返回以node为根的二分搜索树的最小值所在的节点
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        } else {
+            return minimum(node.left);
+        }
+    }
+
+    // 寻找二分搜索树的最大元素
+    public E maximum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+        return maximum(root).e;
+    }
+
+    // 返回以node为根的二分搜索树的最小值所在的节点
+    private Node maximum(Node node) {
+        if (node.right == null) {
+            return node;
+        } else {
+            return maximum(node.right);
+        }
+    }
+
+    // 从二分搜索树中删除最小值所在节点，返回最小值
+    public E removeMin() {
+        E ret = minimum();
+        root = removeMin(root);
+        return ret;
+    }
+
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    // 从二分搜索树中删除最大值所在节点，返回最大值
+    public E removeMax() {
+        E ret = maximum();
+        root = removeMax(root);
+        return ret;
+    }
+
+    private Node removeMax(Node node) {
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+
+        node.right = removeMax(node.right);
+        return node;
     }
 
     @Override
--- test/com/linhuaxin/ds/bst/BSTTest.java
+++ test/com/linhuaxin/ds/bst/BSTTest.java
@@ -1,22 +1,40 @@
 package com.linhuaxin.ds.bst;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class BSTTest {
 
     public static void main(String[] args) {
 
         BST<Integer> bst = new BST<>();
-        int[] nums = {5, 3, 6, 8, 4, 2};
-        for (int num : nums) {
-            bst.add(num);
+//        int[] nums = {5, 3, 6, 8, 4, 2};
+//        for (int num : nums) {
+//            bst.add(num);
+//        }
+
+//        bst.preOrder();
+//        System.out.println();
+//
+//        bst.preOrderNR();
+
+//        bst.inOrder();
+//        System.out.println();
+//
+//        bst.postOrder();
+//        System.out.println();
+
+//        bst.levelOrder();
+
+        Random random = new Random();
+        int n = 1000;
+        for (int i = 0; i < n; i++) {
+            bst.add(random.nextInt(10000));
         }
-
-        bst.preOrder();
-        System.out.println();
-
-        bst.inOrder();
-        System.out.println();
-
-        bst.postOrder();
-        System.out.println();
+        ArrayList<Integer> nums = new ArrayList<>();
+        while (!bst.isEmpty()) {
+            nums.add(bst.removeMax());
+        }
+        System.out.println(nums);
     }
 }
