66ccb6104daf8c003e3115f81ad10fa7d8169529
linhuaxin <linhuaxin@foxmail.com>
2019-02-14 23:20:39 +0800

    Set

--- src/com/linhuaxin/ds/set/LinkedListSet.java
+++ src/com/linhuaxin/ds/set/LinkedListSet.java
@@ -1,4 +1,36 @@
 package com.linhuaxin.ds.set;
 
-public class LinkedListSet<E> {
+import com.linhuaxin.ds.linkedList.LinkedList;
+
+public class LinkedListSet<E> implements Set<E> {
+
+    private LinkedList<E> list;
+
+    public LinkedListSet() {
+        list = new LinkedList<>();
+    }
+    @Override
+    public void add(E e) {
+
+    }
+
+    @Override
+    public void remove(E e) {
+
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 }
