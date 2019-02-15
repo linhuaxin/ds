2ce3c2bdefb7a033917b786a7651d6003d7b37dd
linhuaxin <linhuaxin@foxmail.com>
2019-02-15 16:51:17 +0800

    LinkedListSet

--- src/com/linhuaxin/ds/set/LinkedListSet.java
+++ src/com/linhuaxin/ds/set/LinkedListSet.java
@@ -11,26 +11,28 @@ public class LinkedListSet<E> implements Set<E> {
     }
     @Override
     public void add(E e) {
-
+        if (!list.contains(e)) {
+            list.addFirst(e);
+        }
     }
 
     @Override
     public void remove(E e) {
-
+        list.removeElement(e);
     }
 
     @Override
     public boolean contains(E e) {
-        return false;
+        return list.contains(e);
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return list.getSize();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return list.isEmpty();
     }
 }
