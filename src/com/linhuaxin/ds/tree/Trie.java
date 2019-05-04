a500ff4c759742ddc26a7200a3f245314a331069
linhuaxin <linhuaxin@foxmail.com>
2019-05-04 21:57:42 +0800

    Trie

--- src/com/linhuaxin/ds/tree/Trie.java
+++ src/com/linhuaxin/ds/tree/Trie.java
@@ -36,4 +36,16 @@ public class Trie {
             size++;
         }
     }
+
+    public boolean contains(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
 }
